a90346c5de46c8ead6ceab28e92316629df4ad0f
ElineQ <84079851+ElineQ@users.noreply.github.com>
2021-06-18 11:58:43 +0200

    Add files via upload

---
+++ arguments.js
@@ -0,0 +1,20 @@
+
+const greeting = function visitors
+     console.log('Come in!');
+};
+
+WelcomeIn('Less than hunderd visitors');
+
+const TooMany = function visitors {
+    console.log('Sorry, we are full. Come back later');
+};
+
+TooMany('More than hunderd visitors');
+
+if (age >= 18) {
+    console.log("Come in");
+} else {
+    console.log("You are to young for this club");
+}
+
+
